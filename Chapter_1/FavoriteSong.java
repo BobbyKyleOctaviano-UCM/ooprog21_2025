f7b9bdf74e0740cf2be260e18648f8d53818cc13
BobbyKyleOctaviano-UCM <bobbykyle06@gmail.com>
2025-09-02 22:03:28 +0800

    FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        
+        System.out.println("Swear I couldn't sleep a wink last night");
+        System.out.println("No point in turnin' off the lights");
+        System.out.println("Not the same without your head on my shoulders");
+        System.out.println("Growin' pains, but I don't wanna get older");
+    }
+}
