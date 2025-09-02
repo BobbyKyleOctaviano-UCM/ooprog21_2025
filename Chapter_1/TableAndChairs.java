e27e7108803b50fd233dd5f4c1cc019ef195fa89
BobbyKyleOctaviano-UCM <bobbykyle06@gmail.com>
2025-09-02 21:53:35 +0800

    TableAndChairs.java

---
+++ Chapter_1/TableAndChairs.java
@@ -0,0 +1,28 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        
+        int i;
+        
+        for (i = 1; i <= 5; i++){
+            System.out.println("*                             *");
+        }
+            System.out.print(" ");
+        for( i = 1; i <= 9; i++){
+            System.out.print("_");
+        }
+        System.out.print(" ");
+         for( i = 1; i <= 9; i++){
+            System.out.print("_");
+        }
+                System.out.print(" ");
+         for( i = 1; i <= 9; i++){
+            System.out.print("_");
+        }
+            System.out.println(" ");
+        for ( i = 1; i <= 5; i++){
+            System.out.println("*        * *       * *        *");
+        }
+    
+        
+    }
+}
