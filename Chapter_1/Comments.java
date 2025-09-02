4145d6a273052dad681b870343c680f053093be7
BobbyKyleOctaviano-UCM <bobbykyle06@gmail.com>
2025-09-02 22:33:49 +0800

    Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,17 @@
+public class Comments {
+    public static void main(String[] args) {
+ 
+        System.out.println("Single-line Comment"); 
+        // Used for short notes or quick explanations.
+         //Single-line comment (short notes).
+        System.out.println("Multi-line Comment"); 
+        /* Useful for long explanations or temporarily disabling blocks of code.
+        It can also span across several lines.
+        -Multi-line comment (explanations or disabling code).*/
+        
+        System.out.println("Documentation Comment");
+        /** Supports tags (like @author, @param, @return). 
+         -Documentation comment (Javadoc, structured docs
+        */
+    }
+}
