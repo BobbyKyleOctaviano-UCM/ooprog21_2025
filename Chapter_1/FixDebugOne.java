f525d447ae748f979dfd653c30e010cb5e7a63fa
BobbyKyleOctaviano-UCM <bobbykyle06@gmail.com>
2025-09-03 08:14:16 +0800

    FixDebugOne.java

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,17 @@
+public class FixDebugOne
+{
+/* This program displays a greeting */
+public static void main(String[]  args)
+   {
+   System.out.println("Hello World!");
+   }  
+}
+/*
+Bugs:
+1. remove the space on the "FixDebug One".
+2. need 1 more "bracket" in the access modifier.
+3. Wrong syntax for "Main" instead of "main"
+4. No "[]" in string
+5. Wrong syntax for "Systm" instead of "System".
+6.No "semi colon/;" in output.
+*/
